03131e4f8cbbcee057b0da8e5e41effc893db8d2
TheOnlyOne <stepatheone@gmail.com>
2022-10-01 01:14:56 +0700

    End Lab2

--- Attributes/app/src/main/java/com/example/attributes/MainActivity.kt
+++ Attributes/app/src/main/java/com/example/attributes/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.attributes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.util.TypedValue
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val myText: EditText = findViewById(R.id.myEditText)
+        val button_bl: Button = findViewById(R.id.button_black)
+        val button_rd: Button = findViewById(R.id.button_red)
+        val button_eight: Button = findViewById(R.id.button_8sp)
+        val button_twnt: Button = findViewById(R.id.button24sp)
+        val button_yeBack: Button = findViewById(R.id.button_yellowBack)
+        val button_whBack: Button = findViewById(R.id.button_whiteBack)
+        button_bl.setOnClickListener {
+            myText.setTextColor(Color.BLACK)
+        }
+        button_rd.setOnClickListener {
+            myText.setTextColor(Color.RED)
+        }
+        button_eight.setOnClickListener {
+            myText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 8F)
+        }
+        button_twnt.setOnClickListener {
+            myText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24F)
+        }
+        button_yeBack.setOnClickListener {
+            myText.setBackgroundColor(Color.YELLOW)
+        }
+        button_whBack.setOnClickListener {
+            myText.setBackgroundColor(Color.WHITE)
+        }
     }
 }
