8a4e7207b49adff5b7d25e72696c0c7ab93a4919
TheOnlyOne <stepatheone@gmail.com>
2022-10-09 22:38:09 +0700

    Lab3Full

---
+++ ComplexEvent/app/src/main/java/com/example/complexevent/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.complexevent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button:Button = findViewById(R.id.buttonSave)
+        val check:CheckBox = findViewById(R.id.checkBox)
+        val progressBar:ProgressBar = findViewById(R.id.progressBar)
+        val savedText:TextView = findViewById(R.id.textView)
+        val textNeedtoSave:EditText = findViewById(R.id.editText)
+        button.setOnClickListener {
+            if(check.isChecked){
+                progressBar.progress = progressBar.progress + 10
+                savedText.setText(textNeedtoSave.text)
+            }
+        }
+    }
+}
